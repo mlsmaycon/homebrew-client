7f78f69c586ae49db6ce6e208d9b949b44cba361
mlsmaycon <dev@netbird.io>
2022-05-24 16:39:49 +0000

    Add changes

--- Casks/netbird-ui.rb
+++ Casks/netbird-ui.rb
@@ -3,20 +3,30 @@
 
 # Netbird's UI Client Cask Formula
 cask "netbird-ui" do
-  version "0.5.6"
+  version "0.5.10"
 
   if Hardware::CPU.intel?
-      url "https://github.com/mlsmaycon/netbird/releases/download/v0.5.6/netbird-ui_0.5.6_darwin_amd64_signed.zip"
-      sha256 "e6144d063fbfa838f61383179ca2b2692beaeabf5c2874e612fac058dadb6081"
+      url "https://github.com/mlsmaycon/netbird/releases/download/v0.5.10/netbird-ui_0.5.10_darwin_amd64_signed.zip"
+      sha256 "2bfe32746d155cd8fa1a058bd5ecaad4a327860bfd6a57f1bcd55207650f15b7"
       app "netbird_ui_darwin_amd64", target: "Netbird UI.app"
   else
-      url "https://github.com/mlsmaycon/netbird/releases/download/v0.5.6/netbird-ui_0.5.6_darwin_arm64_signed.zip"
-      sha256 "2d9f07a5d6f2e6a354977e121d7528af6c5b772d1b9f7509837233bfa58e3374"
+      url "https://github.com/mlsmaycon/netbird/releases/download/v0.5.10/netbird-ui_0.5.10_darwin_arm64_signed.zip"
+      sha256 "a2567d7d41119eca239cf95fb9e8e18504ab7543886a2bc74f4bc2c5c3cdafdb"
       app "netbird_ui_darwin_arm64", target: "Netbird UI.app"
   end
 
   depends_on formula: "netbird"
 
+  installer script: {
+    executable: "installer.sh",
+    sudo: true,
+    must_succeed: false,
+  }
+
+  uninstall script: {
+    executable: "uninstaller.sh",
+    args: ["service","stop"],
+  }
 
   name "Netbird UI"
   desc "Netbird UI Client"
